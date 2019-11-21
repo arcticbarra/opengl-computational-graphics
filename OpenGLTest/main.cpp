5a5bdca7948e2d50b7375b817361ca51ce09adde
lorbichara <lorbichara@gmail.com>
2019-11-21 11:11:26 -0600

    Delete commented lines of code

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -60,7 +60,7 @@ void makeCheckImage(void)
     //Generating Checker Patterm
    for (i = 0; i < checkImageHeight; i++) {
       for (j = 0; j < checkImageWidth; j++) {
-         c = ((((i&0x8)==0)^((j&0x8))==0))*255;
+          c = ((((i&0x8)==0)^(((j&0x8))==0)))*255;
          checkImage[i][j][0] = (GLubyte) c;
          checkImage[i][j][1] = (GLubyte) c;
          checkImage[i][j][2] = (GLubyte) c;
@@ -79,12 +79,6 @@ void initGL() {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth,
                  checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE,
                  checkImage);
-    
-//   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-//   glClearDepth(1.0f);                   // Set background depth to farthest
-//   glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
-//   glShadeModel(GL_SMOOTH);   // Enable smooth shading
-//   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
  
 /* Handler for window-repaint event. Called back when the window first appears and
@@ -221,12 +215,6 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
     gluPerspective(90.0f, aspect, 0.1f, 100.0f);
     glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
    glLoadIdentity();
-    
-   
-//   // Set the aspect ratio of the clipping volume to match the viewport
-//   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-//   glLoadIdentity();             // Reset
-
 }
 
 bool scaling = false;
