3b6935ef1d0bcbd4b1aa276819f44db5c51e1058
Enrique Barragan <ebarragan1997@gmail.com>
2019-11-21 10:19:42 -0600

    move figure creation to another file

---
+++ OpenGLTest/figures.h
@@ -0,0 +1,19 @@
+//
+//  figures.h
+//  OpenGLTest
+//
+//  Created by Enrique Barragán on 21/11/19.
+//  Copyright © 2019 Enrique Barragán. All rights reserved.
+//
+
+#ifndef figures_h
+#define figures_h
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+vector<vector<vector<vector<float>>>> create_shapes();
+
+#endif /* figures_h */
